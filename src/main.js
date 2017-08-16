88f7954b9357b6cdcd47212dd2e045ceac757067
Braun Goodson <braun.goodson@digitaslbi.com>
2017-08-15 20:51:56 -0400

    added

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Buefy from 'buefy'
+import App from './App'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+Vue.use(Buefy)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+
+const types = {
+  LIST_RESOURCE: 'LIST_RESOURCE'
+}
+
+const api = {
+  async get (url) {
+    return Vue.http.get(url)
+      .then(r => Promise.resolve(r))
+      .catch(e => Promise.reject(e))
+  }
+}
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {
+    [types.LIST_RESOURCE] (state, resource) {
+      state[resource.prop] = resource.body
+    }
+  },
+  actions: {
+    async listResource ({commit}, config) {
+      return api.get(config.url)
+        .then(({body}) => commit(types.LIST_RESOURCE, {
+          body: body,
+          prop: config.prop
+        }))
+        .catch(e => Promise.reject(e))
+    }
+  }
+})
